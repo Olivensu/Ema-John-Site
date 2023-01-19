e4c8d1e01f729d95c3a18117fae55c9a8795fa15
OliveNsu <tasbiul.hasan@northsouth.edu>
2023-01-19 18:59:43 +0600

    Order summary updated

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -5,16 +5,18 @@ const Cart = (props) => {
     const {cart}= props
     let price = 0;
     let shipping = 0;
+    let quantity = 0;
     for(const product of cart){
-        price = price + product.price;
-        shipping = shipping + product.shipping;
+        quantity = quantity + product.quantity;
+        price = price + product.price * product.quantity;
+        shipping = shipping + product.shipping *product.quantity;
     }
     let tax = (price*0.1);
     const grandTotal = price + shipping + tax;
     return (
         <div className='cart'>
             <h4>Order Summery</h4>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price:${price} </p>
             <p>Total Shipping: ${shipping} </p>
             <p>Tax: ${tax.toFixed(2)}</p>
--- src/component/Header/Shop/Shop.js
+++ src/component/Header/Shop/Shop.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { addToDb } from '../../../utilities/fakedb';
+import { addToDb,getStorecart } from '../../../utilities/fakedb';
 import Cart from '../../Cart/Cart';
 import Product from '../../Product/Product';
 import './Shop.css'
@@ -10,6 +10,19 @@ const Shop = () => {
     useEffect(()=>{
         fetch('products.json').then(res=> res.json()).then(data=>setProducts(data))
     },[]);
+    useEffect(()=>{
+        const storedCart = getStorecart();
+        const saveCart = [];
+        for(const id in storedCart){
+            const addedProduct = products.find(product=> product.id === id)
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                saveCart.push(addedProduct);
+            }
+        }
+        setCart(saveCart);
+    },[products])
 
     const handleAddToCart = (product)=>{
         console.log(product);
